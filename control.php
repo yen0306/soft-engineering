07d138843c7e5d9ebb5af927c6a15e79cb54f458
108213007 <s108213007@mail1.ncnu.edu.tw>
2022-01-22 02:42:12 +0800

    剩開獎結果

--- control.php
+++ control.php
@@ -7,7 +7,7 @@ if (isset($_REQUEST['act'])) {
 } else $act='';
 
 switch ($act) {
-    case "addUser":
+    case "addUser":  // 新增使用者資訊
         $userName = $_POST['userName'];
         $pwd = $_POST['pwd'];
         if ($userName!="" && $pwd!="") {
@@ -20,7 +20,7 @@ switch ($act) {
             echo "<script>window.location.href='registerUI.html';</script>";
             break;
         }
-    case "login":
+    case "login":  // 登入
         $userName = $_POST['userName'];
         $pwd = $_POST['pwd'];
         $list = getUser($userName);
@@ -33,22 +33,44 @@ switch ($act) {
             echo "<script>window.location.href='loginUI.html';</script>";
             break;
         }
-    case "getRank":
+    case "getUser":  // 得到使用者資訊
+        $list = getUser($_SESSION['userName']);
+        echo json_encode($list);
+        break;
+    case "getRank":  // 得到排行榜
         $rankList = getRank();
         echo json_encode($rankList);
         break;
-    case "addRoom":
+    case "addRoom":  // 新增房間資訊
         $answer = (int)$_POST['answer'];
         addRoom($_SESSION['userName'], $answer, $type);
         header("Location: draw.html");
         break;
-    case "getAnswer":
+    case "getAnswer":  // 得到莊家設定的數字
         $answer = getAnswer($_SESSION['userName']);
         echo json_encode($answer);
         break;
-    case "getRoom" :
+    case "getRoom":  // 得到房間資訊
         $room = getRoom();
         echo json_encode($room);
+        break;
+    case "addPlayer":  // 新增玩家資訊
+        $betNum = (int)$_POST['betNum'];
+        $betMoney = (int)$_POST['betMoney'];
+        $rid = (int)$_POST['rid'];
+        $list = getUser($_SESSION['userName']);
+        if ($list['money'] >= $betMoney) {
+            addPlayer($_SESSION['userName'], $betNum, $betMoney, $rid);
+            header("Location: wait.html?rid=" . (string)$rid);
+            break;
+        } else {
+            echo "<script type='text/javascript'>alert('餘額不足');</script>";
+            echo "<script>window.location.href='player.html?rid=" . (string)$rid . "';</script>";
+            break;
+        }
+    case "getPlayer":  // 得到在那間房間有下注的玩家資訊
+        $list = getPlayer($_SESSION['userName']);
+        echo json_encode($list);
         break;
     default;
 }
--- model.php
+++ model.php
@@ -1,7 +1,7 @@
 <?php
 require("dbconfig.php");
 
-function addUser($userName, $pwd) {
+function addUser($userName, $pwd) {  // 新增使用者資訊
     global $db;
     $money = 1000;
     $sql = "insert into user (userName, pwd, money) values (?, ?, ?)";
@@ -10,7 +10,7 @@ function addUser($userName, $pwd) {
     mysqli_stmt_execute($stmt);
     return true;
 }
-function getUser($userName) {
+function getUser($userName) {  // 得到使用者資訊
     global $db;
     $sql = "select * from user where userName=?";
     $stmt = mysqli_prepare($db, $sql);
@@ -20,7 +20,7 @@ function getUser($userName) {
     $rs = mysqli_fetch_assoc($result);
     return $rs;
 }
-function getRank() {
+function getRank() {  // 得到排行榜
     global $db;
     $sql = "select * from user order by money desc";
     $stmt = mysqli_prepare($db, $sql);
@@ -35,7 +35,7 @@ function getRank() {
     }
     return $retArr;
 }
-function addRoom($userName, $answer, $type=0) {
+function addRoom($userName, $answer, $type=0) {  // 新稱房間使用者
     global $db;
     $sql = "insert into room (userName, answer) values(?, ?)";
     $stmt = mysqli_prepare($db, $sql);
@@ -43,7 +43,7 @@ function addRoom($userName, $answer, $type=0) {
     mysqli_stmt_execute($stmt);
     return true;
 }
-function getAnswer($userName) {
+function getAnswer($userName) {  // 得到莊家設定的數字
     global $db;
     $sql = "select * from room where userName = ?";
     $stmt = mysqli_prepare($db, $sql);
@@ -53,7 +53,7 @@ function getAnswer($userName) {
     $rs = mysqli_fetch_assoc($result);
     return $rs;
 }
-function getRoom() {
+function getRoom() {  // 得到房間資訊
     global $db;
     $sql = "select * from room";
     $stmt = mysqli_prepare($db, $sql);
@@ -64,6 +64,30 @@ function getRoom() {
         $tArr=array();
         $tArr['rid']=$rs['rid'];
         $tArr['userName']=$rs['userName'];
+        $retArr[] = $tArr;
+    }
+    return $retArr;
+}
+function addPlayer($userName, $betNum, $betMoney, $rid) {  // 新增玩家資訊
+    global $db;
+    $sql = "insert into player (userName, rid, betNum, betMoney) values (?, ?, ?, ?)";
+    $stmt = mysqli_prepare($db, $sql);
+    mysqli_stmt_bind_param($stmt, "siii", $userName, $rid, $betNum, $betMoney);
+    mysqli_stmt_execute($stmt);
+    return true;
+}
+function getPlayer($userName) {  // 得到在那間房間有下注的玩家資訊
+    global $db;
+    $sql = "select * from player where rid = (select rid from room where userName = ?)";
+    $stmt = mysqli_prepare($db, $sql);
+    mysqli_stmt_bind_param($stmt, "s", $userName);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    $retArr=array();
+    while ($rs = mysqli_fetch_assoc($result)) {
+        $tArr=array();
+        $tArr['userName']=$rs['userName'];
+        $tArr['betMoney']=$rs['betMoney'];
         $retArr[] = $tArr;
     }
     return $retArr;
